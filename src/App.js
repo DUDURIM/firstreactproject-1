f75875e160104fa909762c16ec64659db91986e2
GuyeongJung <dudurim74@gmail.com>
2022-07-14 17:55:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="jumbotron-text-center">
+  <h1>나의 첫 리액트 프로젝트</h1>
+  <p>나만의 리액트 페이지를 만들어보아요</p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>배워보기</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>느껴보기</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>배포하기</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>배포하기</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>배포하기</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>배포하기</p>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
